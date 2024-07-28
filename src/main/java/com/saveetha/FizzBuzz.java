cbe8057d9c91a79f66977e535a6c008e5b338c4a
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-28 19:34:35 +0530

    UPDATE. For Better practice, instead checking the values in the if condition, created private method to check it.

--- src/main/java/com/saveetha/FizzBuzz.java
+++ src/main/java/com/saveetha/FizzBuzz.java
@@ -3,15 +3,26 @@ package com.saveetha;
 public class FizzBuzz {
 
     public String of(int number) {
-        if(number % 15 == 0) {
+        if( isNumberDivisibleByFifteen(number) ) {
             return "FizzBuzz";
         }
-        if(number % 3 == 0) {
+        if( isNumberDivisibleByThree(number) ) {
             return "Fizz";
         }
-        if(number % 5 == 0) {
+        if( isNumberDivisibleByFive(number) ) {
             return "Buzz";
         }
         return String.valueOf(number);
     }
+
+    private static boolean isNumberDivisibleByThree(int number) {
+        return number % 3 == 0;
+    }
+    private static boolean isNumberDivisibleByFive(int number) {
+        return number % 5 == 0;
+    }
+    private static boolean isNumberDivisibleByFifteen(int number) {
+        return number % 15 == 0;
+    }
+
 }
