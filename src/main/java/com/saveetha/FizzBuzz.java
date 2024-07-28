48172c7c8b13c29e02a1b028836745e5fb79560b
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-28 19:02:23 +0530

    ADD. First Test, to return the number as it is.

--- src/main/java/com/saveetha/FizzBuzz.java
+++ src/main/java/com/saveetha/FizzBuzz.java
@@ -2,4 +2,7 @@ package com.saveetha;
 
 public class FizzBuzz {
 
+    public int findFizzBuzz(int number) {
+        return number;
+    }
 }
--- src/test/java/com/saveetha/FizzBuzzTest.java
+++ src/test/java/com/saveetha/FizzBuzzTest.java
@@ -1,6 +1,18 @@
 package com.saveetha;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
 
+    @Test
+    void toReturnOneWhenTheNumberIsOne() {
+        int one = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
 
+        int actualValue = fizzBuzz.findFizzBuzz(one);
+
+        assertEquals(one, actualValue);
+    }
 }
